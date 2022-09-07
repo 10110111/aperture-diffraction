0adc45b7abd2b6a224405cc280860309f1ab2ffc
Ruslan Kabatsayev <b7.10110111@gmail.com>
2022-09-07 18:46:48 +0300

    Represent *clockwise* rotation with positive angle values

--- ToolsWidget.hpp
+++ ToolsWidget.hpp
@@ -13,7 +13,7 @@ public:
 
     double exposure() const { return exposure_->value(); }
     double scale() const { return scale_->value(); }
-    double globalRotationAngle() const { return globalRotationAngle_->value()*std::acos(-1.)/180; }
+    double globalRotationAngle() const { return globalRotationAngle_->value()*-std::acos(-1.)/180; }
     int pointCount() const { return pointCount_->value(); }
     int arcPointCount() const { return arcPointCount_->value(); }
     double curvatureRadius() const { return curvatureRadius_->value(); }
