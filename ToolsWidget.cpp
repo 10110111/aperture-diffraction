eef8df83e31789fccc80f08167a68c45b587df88
Ruslan Kabatsayev <b7.10110111@gmail.com>
2022-09-07 18:45:34 +0300

    Use better defaults and limits

--- ToolsWidget.cpp
+++ ToolsWidget.cpp
@@ -21,13 +21,13 @@ ToolsWidget::ToolsWidget(QWidget* parent)
     mainWidget->setLayout(layout);
     setWidget(mainWidget);
 
-    exposure_ = addManipulator(layout, this, tr(u8"log<sub>10</sub>e&xposure"), -8, 7, 0, 2);
-    scale_    = addManipulator(layout, this, tr(u8"log<sub>10</sub>&scale"), -5, 1, -2.5, 2);
-    globalRotationAngle_ = addManipulator(layout, this, tr(u8"&Rotation angle"), -90, 90, 0, 4, u8"°");
-    pointCount_ = addManipulator(layout, this, tr(u8"Aperture ed&ge count"), 3, 100, 6, 0);
-    arcPointCount_ = addManipulator(layout, this, tr(u8"&Points per arc (side)"), 0, 100, 0, 0);
-    curvatureRadius_ = addManipulator(layout, this, tr(u8"Radius of &curvature of side"), 1, 50, 50, 2);
-    sampleCount_ = addManipulator(layout, this, tr(u8"Sa&mples per pixel side"), 1, 99, 1, 0);
+    exposure_ = addManipulator(layout, this, tr(u8"log<sub>10</sub>e&xposure"), -8, 7, -2, 2);
+    scale_    = addManipulator(layout, this, tr(u8"log<sub>10</sub>&scale"), -5, 1, -3, 2);
+    globalRotationAngle_ = addManipulator(layout, this, tr(u8"&Rotation angle"), -90, 90, 14, 4, u8"°");
+    pointCount_ = addManipulator(layout, this, tr(u8"Aperture ed&ge count"), 3, 99, 6, 0);
+    arcPointCount_ = addManipulator(layout, this, tr(u8"&Points per arc (side)"), 0, 99, 25, 0);
+    curvatureRadius_ = addManipulator(layout, this, tr(u8"Radius of &curvature of side"), 1, 50, 3, 2);
+    sampleCount_ = addManipulator(layout, this, tr(u8"Sa&mples per pixel side"), 1, 19, 1, 0);
     wavelengthCount_ = addManipulator(layout, this, tr(u8"Number of &wavelengths"), 2, 9999, 256, 0);
 
     layout->addStretch();
