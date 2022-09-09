80a3c899a7ad24dbe72f6084cc1c922d0fb30a1b
Ruslan Kabatsayev <b7.10110111@gmail.com>
2022-09-09 16:25:48 +0300

    Use better accelerator keys

--- ToolsWidget.cpp
+++ ToolsWidget.cpp
@@ -24,9 +24,9 @@ ToolsWidget::ToolsWidget(QWidget* parent)
     exposure_ = addManipulator(layout, this, tr(u8"log<sub>10</sub>e&xposure"), -8, 7, -2, 2);
     scale_    = addManipulator(layout, this, tr(u8"log<sub>10</sub>&scale"), -5, 1, -3, 2);
     globalRotationAngle_ = addManipulator(layout, this, tr(u8"&Rotation angle"), -90, 90, 14, 4, u8"°");
-    pointCount_ = addManipulator(layout, this, tr(u8"Aperture ed&ge count"), 3, 99, 6, 0);
+    pointCount_ = addManipulator(layout, this, tr(u8"&Aperture edge count"), 3, 99, 6, 0);
     arcPointCount_ = addManipulator(layout, this, tr(u8"&Points per arc (side)"), 0, 99, 25, 0);
-    curvatureRadius_ = addManipulator(layout, this, tr(u8"Radius of &curvature of side"), 1, 50, 3, 2);
+    curvatureRadius_ = addManipulator(layout, this, tr(u8"Ra&dius of curvature of side"), 1, 50, 3, 2);
     sampleCount_ = addManipulator(layout, this, tr(u8"Sa&mples per pixel side"), 1, 19, 1, 0);
     wavelengthCount_ = addManipulator(layout, this, tr(u8"Number of &wavelengths"), 1, 9999, 256, 0);
 
