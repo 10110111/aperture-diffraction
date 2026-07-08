98c2a5997d711cd808d25de1590fec09f3874dc4
Ruslan Kabatsayev <b7.10110111@gmail.com>
2026-07-08 10:32:19 +0800

    Fix useless repetition of aperture outline points

--- ApertureOutline.cpp
+++ ApertureOutline.cpp
@@ -106,39 +106,32 @@ void ApertureOutline::paintGL()
     const auto sqr = [](auto x){ return x*x; };
     const float PI = acos(-1.);
 
-    const int sampleCount = tools_->sampleCount();
     const int pointCount = tools_->pointCount();
     const float globalRotationAngle = tools_->globalRotationAngle();
     const float curvatureRadius = tools_->curvatureRadius();
     const float arcPointCount = tools_->arcPointCount();
 
     std::vector<vec2> vertices;
-    for(int sampleNumY=0; sampleNumY<sampleCount; ++sampleNumY)
+    for(int pointNum=1; pointNum<=pointCount; ++pointNum)
     {
-        for(int sampleNumX=0; sampleNumX<sampleCount; ++sampleNumX)
+        float phi1 = 2*PI*(pointNum-1)/pointCount + (pointCount%2==1 ? PI/2 : 0) + globalRotationAngle;
+        float phi2 = 2*PI* pointNum   /pointCount + (pointCount%2==1 ? PI/2 : 0) + globalRotationAngle;
+        vec2 p1=vec2(cos(phi1), sin(phi1));
+        vec2 p2=vec2(cos(phi2), sin(phi2));
+        vec2 midPoint = (p1+p2)/2.f;
+        float arcCenterDistFromMid = sqrt(sqr(curvatureRadius)-dot(p1-p2,p1-p2)/4);
+        vec2 arcCenter = midPoint * (1-arcCenterDistFromMid/length(midPoint));
+        float arcPhi1 = atan(p1.y-arcCenter.y, p1.x-arcCenter.x);
+        float arcPhi2 = atan(p2.y-arcCenter.y, p2.x-arcCenter.x);
+        if(arcPhi2<arcPhi1) arcPhi2 += 2*PI;
+        for(float arcPointNum=0; arcPointNum<=arcPointCount; ++arcPointNum)
         {
-            for(int pointNum=1; pointNum<=pointCount; ++pointNum)
-            {
-                float phi1 = 2*PI*(pointNum-1)/pointCount + (pointCount%2==1 ? PI/2 : 0) + globalRotationAngle;
-                float phi2 = 2*PI* pointNum   /pointCount + (pointCount%2==1 ? PI/2 : 0) + globalRotationAngle;
-                vec2 p1=vec2(cos(phi1), sin(phi1));
-                vec2 p2=vec2(cos(phi2), sin(phi2));
-                vec2 midPoint = (p1+p2)/2.f;
-                float arcCenterDistFromMid = sqrt(sqr(curvatureRadius)-dot(p1-p2,p1-p2)/4);
-                vec2 arcCenter = midPoint * (1-arcCenterDistFromMid/length(midPoint));
-                float arcPhi1 = atan(p1.y-arcCenter.y, p1.x-arcCenter.x);
-                float arcPhi2 = atan(p2.y-arcCenter.y, p2.x-arcCenter.x);
-                if(arcPhi2<arcPhi1) arcPhi2 += 2*PI;
-                for(float arcPointNum=0; arcPointNum<=arcPointCount; ++arcPointNum)
-                {
-                    float angle1=arcPhi1+(arcPhi2-arcPhi1)* arcPointNum   /(arcPointCount+1);
-                    float angle2=arcPhi1+(arcPhi2-arcPhi1)*(arcPointNum+1)/(arcPointCount+1);
-                    vec2 arcP1 = arcCenter + curvatureRadius*vec2(cos(angle1), sin(angle1));
-                    vec2 arcP2 = arcCenter + curvatureRadius*vec2(cos(angle2), sin(angle2));
-                    vertices.push_back(arcP1);
-                    vertices.push_back(arcP2);
-                }
-            }
+            float angle1=arcPhi1+(arcPhi2-arcPhi1)* arcPointNum   /(arcPointCount+1);
+            float angle2=arcPhi1+(arcPhi2-arcPhi1)*(arcPointNum+1)/(arcPointCount+1);
+            vec2 arcP1 = arcCenter + curvatureRadius*vec2(cos(angle1), sin(angle1));
+            vec2 arcP2 = arcCenter + curvatureRadius*vec2(cos(angle2), sin(angle2));
+            vertices.push_back(arcP1);
+            vertices.push_back(arcP2);
         }
     }
 
