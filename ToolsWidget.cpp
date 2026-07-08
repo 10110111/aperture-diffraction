b7e4835c5029b3cd7dc56152a461cabcd02ebdd6
Ruslan Kabatsayev <b7.10110111@gmail.com>
2026-07-08 10:29:58 +0800

    Make some sliders nonlinear

--- ToolsWidget.cpp
+++ ToolsWidget.cpp
@@ -2,11 +2,11 @@
 #include "Manipulator.hpp"
 #include <QPushButton>
 
-static Manipulator* addManipulator(QVBoxLayout*const layout, ToolsWidget*const tools,
-                                   QString const& label, const double min, const double max, const double defaultValue,
-                                   const int decimalPlaces, QString const& unit="")
+Manipulator* addManipulator(QVBoxLayout*const layout, ToolsWidget*const tools,
+                            QString const& label, const double min, const double max, const double defaultValue,
+                            const int decimalPlaces, QString const& unit="", const bool nonlinearSlider=false)
 {
-    const auto manipulator=new Manipulator(label, min, max, defaultValue, decimalPlaces);
+    const auto manipulator=new Manipulator(label, min, max, defaultValue, decimalPlaces, nonlinearSlider);
     layout->addWidget(manipulator);
     tools->connect(manipulator, &Manipulator::valueChanged, tools, &ToolsWidget::settingChanged);
     if(!unit.isEmpty())
@@ -27,8 +27,8 @@ ToolsWidget::ToolsWidget(QWidget* parent)
     globalRotationAngle_ = addManipulator(layout, this, tr(u8"&Rotation angle"), -90, 90, 14, 4, u8"°");
     pointCount_ = addManipulator(layout, this, tr(u8"&Aperture edge count"), 3, 99, 6, 0);
     arcPointCount_ = addManipulator(layout, this, tr(u8"&Points per arc (side)"), 0, 99, 25, 0);
-    apertureRadius_ = addManipulator(layout, this, tr(u8"Rad&ius of aperture"), 0.1, 50, 1, 2, tr(" mm"));
-    curvatureRadius_ = addManipulator(layout, this, tr(u8"Ra&dius of curvature of side"), 1, 50, 3, 2, tr(u8" Rₐₚₜ"));
+    apertureRadius_ = addManipulator(layout, this, tr(u8"Rad&ius of aperture"), 0.1, 50, 1, 2, tr(" mm"), true);
+    curvatureRadius_ = addManipulator(layout, this, tr(u8"Ra&dius of curvature of side"), 1, 50, 3, 2, tr(u8" Rₐₚₜ"), true);
     sampleCount_ = addManipulator(layout, this, tr(u8"Sa&mples per pixel side"), 1, 19, 1, 0);
     wavelengthCount_ = addManipulator(layout, this, tr(u8"Number of &wavelengths"), 1, 9999, 256, 0);
 #if QT_VERSION >= QT_VERSION_CHECK(6,2,0) // Requires QImage::Format_RGBX32FPx4
