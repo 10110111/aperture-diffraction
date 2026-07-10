345ef06684290cf566c8f932b4487f47d3d6c363
Ruslan Kabatsayev <b7.10110111@gmail.com>
2026-07-10 13:02:49 +0800

    Make wavelength count slider nonlinear

--- ToolsWidget.cpp
+++ ToolsWidget.cpp
@@ -30,7 +30,7 @@ ToolsWidget::ToolsWidget(QWidget* parent)
     apertureRadius_ = addManipulator(layout, this, tr(u8"Rad&ius of aperture"), 0.1, 50, 1, 2, tr(" mm"), true);
     curvatureRadius_ = addManipulator(layout, this, tr(u8"Ra&dius of curvature of side"), 1, 50, 3, 2, tr(u8" Rₐₚₜ"), true);
     sampleCount_ = addManipulator(layout, this, tr(u8"Sa&mples per pixel side"), 1, 19, 1, 0);
-    wavelengthCount_ = addManipulator(layout, this, tr(u8"Number of &wavelengths"), 1, 9999, 256, 0);
+    wavelengthCount_ = addManipulator(layout, this, tr(u8"Number of &wavelengths"), 1, 9999, 256, 0, "", true);
 #if QT_VERSION >= QT_VERSION_CHECK(6,2,0) // Requires QImage::Format_RGBX32FPx4
     saveBtn_ = new QPushButton(tr("Sa&ve image"));
     layout->addWidget(saveBtn_);
